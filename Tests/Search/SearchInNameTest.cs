0d2d3e17dcbed8668509ab191fcd48751a5785b8
Sofiia Levinska <sofialevinska@gmail.com>
2021-02-02 13:49:05 +0200

    Levinska_Search_InitialCommit

---
+++ Tests/Search/SearchInNameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using SeleniumPageObject.Tests;
+using SeleniumPageObject.Steps;
+
+namespace MagentoLv553SET.Tests.Search
+{
+    [TestFixture]
+    public class SearchInNameTest : BaseTest
+    {
+
+        [Test]
+        public void AuthorizationTest()
+        {
+            var homePage = new HomePageBL(webDriver);
+            homePage
+                .ClickOnMainSignInButton();
+
+
+            Assert.IsTrue();
+        }
+    }
+}
